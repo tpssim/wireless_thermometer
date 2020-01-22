ca0ada2cebd874ad7ce041e73cdc0c8347674405
tpssim <teemu.simola@outlook.com>
2020-01-22 15:40:45 +0200

    wireless communication for display

--- display/main.py
+++ display/main.py
@@ -1,69 +1,22 @@
-from machine import Pin, I2C
-from time import sleep
-import ssd1306
+import network
+import usocket as socket
+import ujson as json
 
-#amount of connected sensors(1 or 2)
-SENSORS = 1
+ESSID = 'temp_display'
+PASSW = '88888888'
 
-sens1_name = 'Sensor 1'
-sens1_temp = 0.0
-sens1_pa = 0.0
-sens1_hum = 0.0
+ap_if = network.WLAN(network.AP_IF)
+ap_if.config(essid=ESSID, password=PASSW, hidden=True)
+ap_if.ifconfig(('192.168.0.4', '255.255.255.0', '192.168.0.1', '1.1.1.1'))
 
-sens2_name = 'Sensor 2'
-sens2_temp = 0.0
-sens2_pa = 0.0
-sens2_hum = 0.0
+addr = socket.getaddrinfo('0.0.0.0', 8080)[0][-1]
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+print('listening on:', addr)
 
-oled_width = 128
-oled_heigth = 64
-
-#Init the display
-i2c = I2C(-1, scl=Pin(5), sda=Pin(4))
-oled = ssd1306.SSD1306_I2C(oled_width, oled_heigth, i2c)
-
-if(SENSORS == 1):
-    #Title
-    oled.text(sens1_name, 0, 4)
-
-    #Sensor values
-    #With only one sensor there is plenty of room on the screen
-    oled.text('Temp. ' + str(sens1_temp) + 'C', 0, 20)
-    oled.text('PA    ' + str(sens1_pa) + 'hPa', 0, 36)
-    oled.text('Hum.  ' + str(sens1_hum) + '%', 0, 52)
-
-elif(SENSORS == 2):
-    #Split the screen
-    oled.vline(64, 0, 64, 1)
-    #Titles
-    oled.text(sens1_name, 0, 4)
-    oled.text(sens2_name, 65, 4)
-
-    #Sensor 1 values
-    #Max length of any value is 5 caracters
-    if(sens1_temp < 0):
-        oled.text('T.' + str(sens1_temp), 0, 20)
-    else:
-        oled.text('T. ' + str(sens1_temp), 0, 20)
-    if(sens1_pa >= 1000):
-        oled.text('PA ' + str(round(sens1_pa)), 0, 36)
-    else:
-        oled.text('PA ' + str(round(sens1_pa, 1)), 0, 36)   
-    #Humidity can't be more than 5 caracters long
-    oled.text('H. ' + str(sens1_hum), 0, 52)
-
-    #Sensor 2 values
-    #Max length of any value is 5 caracters
-    if(sens2_temp < 0):
-        oled.text('T.' + str(sens2_temp), 65, 20)
-    else:
-        oled.text('T. ' + str(sens2_temp), 65, 20)
-    if(sens2_pa >= 1000):
-        oled.text('PA ' + str(round(sens2_pa)), 65, 36)
-    else:
-        oled.text('PA ' + str(round(sens2_pa, 1)), 65, 36)   
-    #Humidity can't be more than 5 caracters long
-    oled.text('H. ' + str(sens2_hum), 65, 52)
-
-
-oled.show()
+cl, addr = s.accept()
+print('client connected from:', addr)
+receive = cl.read()
+data = json.loads(receive)
+print('Received data:', data)
